0a42ba444237f6734faf1db49860b4a5a93d84b2
EXL <exlmotodev@gmail.com>
2020-11-25 02:24:03 +0700

    Initial commit

---
+++ src/main/java/ru/exlmoto/code/CodeServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.exlmoto.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/exlmoto/code/CodeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.exlmoto.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
