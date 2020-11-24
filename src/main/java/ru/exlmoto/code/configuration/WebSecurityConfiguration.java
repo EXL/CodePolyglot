4c72374b92bb0012c678bb6668fe2579613be2ea
EXL <exlmotodev@gmail.com>
2020-11-25 03:24:51 +0700

    Permit all paths

---
+++ src/main/java/ru/exlmoto/code/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,13 @@
+package ru.exlmoto.code.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().permitAll();
+	}
+}
