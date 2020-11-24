7b053d6e12d246648c7e2815fa881734e9fbe8ee
EXL <exlmotodev@gmail.com>
2020-11-25 03:45:58 +0700

    Add RestContoller

---
+++ src/main/java/ru/exlmoto/code/controller/CodeController.java
@@ -0,0 +1,17 @@
+package ru.exlmoto.code.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.graalvm.polyglot.*;
+
+@RestController
+public class CodeController {
+	// Context polyglot = Context.create();
+
+	@GetMapping(value = "/", produces = "text/plain")
+	public String test() {
+		String js = "JS: ";
+		return "";
+	}
+}
