5cfd081a3fc435fe6aca3814ef0917b36e75858c
EXL <exlmotodev@gmail.com>
2020-11-25 03:12:42 +0700

    Update skeleton project

--- src/main/java/ru/exlmoto/code/CodeServiceApplication.java
+++ src/main/java/ru/exlmoto/code/CodeServiceApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CodeServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CodeServiceApplication.class, args);
 	}
-
 }
--- src/test/java/ru/exlmoto/code/CodeServiceApplicationTests.java
+++ src/test/java/ru/exlmoto/code/CodeServiceApplicationTests.java
@@ -1,13 +1,19 @@
 package ru.exlmoto.code;
 
 import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class CodeServiceApplicationTests {
+	@Autowired
+	private CodeServiceApplication codeServiceApplication;
 
 	@Test
 	void contextLoads() {
+		assertNotNull(codeServiceApplication);
 	}
-
 }
