6956580e9eea34d50b8cdf4e98ae0603ef05f9e6
EXL <exlmotodev@gmail.com>
2020-11-29 16:23:48 +0700

    Toggle wrap on/off

--- src/main/resources/static/js/code.js
+++ src/main/resources/static/js/code.js
@@ -2,4 +2,10 @@ document.getElementById('code-area').onkeydown = function (event) {
 	if ((event.ctrlKey || event.metaKey) && (event.key === 'Enter')) {
 		document.getElementById('code-form').submit();
 	}
+	if ((event.key === 'l')) {
+		document.querySelectorAll('.code-table-code').forEach(line => line.style.whiteSpace = 'pre-wrap')
+	}
+	if ((event.key === 'r')) {
+		document.querySelectorAll('.code-table-code').forEach(line => line.style.whiteSpace = 'pre')
+	}
 }
