c94f41d2a39a971bb5142fa975fda9ea383c489f
EXL <exlmotodev@gmail.com>
2020-12-23 22:59:21 +0700

    Disable all languages for Highlight service

--- src/main/java/ru/exlmoto/code/highlight/HighlightService.java
+++ src/main/java/ru/exlmoto/code/highlight/HighlightService.java
@@ -54,7 +54,7 @@ import static ru.exlmoto.code.highlight.enumeration.Mode.HighlightJs;
 import static ru.exlmoto.code.highlight.enumeration.Mode.HighlightRouge;
 import static ru.exlmoto.code.highlight.enumeration.Mode.HighlightPygments;
 //import static ru.exlmoto.code.highlight.enumeration.Mode.HighlightPygmentsJython;
-
+/*
 @Service
 public class HighlightService {
 	private final Logger log = LoggerFactory.getLogger(HighlightService.class);
@@ -208,7 +208,7 @@ public class HighlightService {
 		}
 	}
 }
-/*
+*/
 // Stub service class.
 @Service
 public class HighlightService {
@@ -234,4 +234,3 @@ public class HighlightService {
 		return code;
 	}
 }
-*/
